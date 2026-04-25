932b7937ed3a2509e6aadc16e0fa394742521b8f
Il-Moro <filmor04@gmail.com>
2026-04-25 11:10:33 +0200

    25 Apr 2026: Test OpenBoard + implementazione mosse orizzontali e verticali

--- app/src/main/java/org/chess/pieces/King.java
+++ app/src/main/java/org/chess/pieces/King.java
@@ -10,6 +10,20 @@ public class King extends Piece{
 
     // private boolean firstMove;
 
+    public King(String colour){
+        
+        this.value = Integer.MAX_VALUE;
+        // this.firstMove = true;
+        
+        if(colour == "White"){
+            this.position = new Position(5,1);
+            this.colour = "White";
+        } else if(colour == "Black") {
+            this.position = new Position(5,8);
+            this.colour = "Black";
+        }
+    }
+
     @Override
     public Set<Position> getLegalMoves(ChessBoard chessBoard, Position position) {
     
