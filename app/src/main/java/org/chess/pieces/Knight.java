96c566a48396db7272cc15a5df314dfbd6995485
Il-Moro <filmor04@gmail.com>
2026-04-27 11:09:23 +0200

    27 Apr 2026: Test 1 + implementazione mosse cavallo

--- app/src/main/java/org/chess/pieces/Knight.java
+++ app/src/main/java/org/chess/pieces/Knight.java
@@ -1,5 +1,6 @@
 package org.chess.pieces;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.chess.dataTypes.*;
@@ -7,13 +8,22 @@ import org.chess.organization.*;
 
 public class Knight extends Piece{
 
-    public Knight(String colour){
-        super(colour);
+    public Knight(Position position, String colour){
+        super(position, colour);
     }
 
     @Override
     public Set<Position> getLegalMoves(ChessBoard chessBoard) {
-    
-        return null;
+        Set<Position> moves = new HashSet<Position>();
+
+        int pieceColumn = this.getPosition().column();
+        int pieceRow = this.getPosition().row();
+
+        int[][] rules = {{2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}};
+        
+        for(int r[] : rules){
+            moves.add(new Position(pieceRow + r[0], pieceColumn + r[1]));    
+        }
+        return moves;
     }
 }
