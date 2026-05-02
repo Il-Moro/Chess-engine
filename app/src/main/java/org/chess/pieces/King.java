38a62b2b0f8a1a1a07307bd64b5094a99a66fe39
Filippo Morello <78764982+Il-Moro@users.noreply.github.com>
2026-05-02 15:32:52 +0200

    Revert "Feature/pieces and movements"

--- app/src/main/java/org/chess/pieces/King.java
+++ app/src/main/java/org/chess/pieces/King.java
@@ -1,6 +1,5 @@
 package org.chess.pieces;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import org.chess.dataTypes.*;
@@ -9,30 +8,11 @@ import org.chess.organization.*;
 
 public class King extends Piece{
 
-    public King(Position position, String colour){
-    
-        super(position, colour);
-
-        this.value = Integer.MAX_VALUE;
-    }
+    // private boolean firstMove;
 
     @Override
-    public Set<Position> getPotentialMoves(ChessBoard board) {
-        
-        Set<Position> moves = new HashSet<>();
-
-        int[][] directions = {{1,-1},{1,0},{1,1},{0,1},{-1,1},{-1,0,},{-1,-1},{0,-1}};
-
-        for(int[] d : directions){
-            int targetRow = this.position.row() + d[0];
-            int targetColumn = this.position.column() + d[1];
-
-            Position targetPosition = new Position(targetRow, targetColumn);
-
-            if(0 <= targetRow && targetRow < 8 && 0 <= targetColumn && targetColumn < 8){   
-                moves.add(targetPosition);
-            }
-        }
-        return moves;
+    public Set<Position> getLegalMoves(ChessBoard chessBoard, Position position) {
+    
+        return MoveCalculator.movesOfKing(chessBoard, position);
     }
 }
