500b491c17b723a3750e9650547c98b3f7f221ab
SasaPahor <sasa.pahor@studenti.units.it>
2026-05-23 11:11:10 +0200

    Player changes

--- app/src/main/java/org/chess/organization/Player.java
+++ app/src/main/java/org/chess/organization/Player.java
@@ -5,33 +5,33 @@ import java.util.List;
 
 import org.chess.dataTypes.Position;
 
-    public abstract class Player {
+public abstract class Player {
 
-        public final String playerName;
-        public ChessBoard board;
-        public Piece[][] tiles;
-        public boolean turn=false;
+    public final String playerName;
+    public ChessBoard board;
+    public Piece[][] tiles;
+    public boolean turn=false;
 
-        public Player(String playerName,String color){
-            if (playerName == null || playerName.trim().isEmpty()) {
-                throw new IllegalArgumentException("The name can' t be null or empty");
-            }
-            this.playerName=playerName;
-            this.tiles=board.getBoard();
+    public Player(String playerName,String color){
+        if (playerName == null || playerName.trim().isEmpty()) {
+            throw new IllegalArgumentException("The name can' t be null or empty");
         }
-
-        public String getName(){
-            return playerName;
-        }
-
-        public void moveSelectedPiece(Piece selectedPiece, Position to){//TODO: return bool not void
-            /*
-            if(board.movePiece(selectedPiece,to)){
-                turn=false;
-            }
-            */
-        }
-
-        public abstract List<Piece> getActivePieces(Piece[][] tiles);
-        public abstract List<Piece> getEatenPieces(Piece[][] tiles);
+        this.playerName=playerName;
+        this.tiles=board.getBoard();
     }
+
+    public String getName(){
+        return playerName;
+    }
+
+    public void moveSelectedPiece(Piece selectedPiece, Position to){//TODO: return bool not void
+        /*
+        if(board.movePiece(selectedPiece,to)){
+            turn=false;
+        }
+        */
+    }
+
+    public abstract List<Piece> getActivePieces(Piece[][] tiles);
+    public abstract List<Piece> getEatenPieces(Piece[][] tiles);
+}
