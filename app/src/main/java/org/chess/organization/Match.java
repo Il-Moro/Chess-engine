d8994cb649f5b942b84c19494b21b7b87e148c6a
SasaPahor <sasa.pahor@studenti.units.it>
2026-05-28 13:23:50 +0200

    Defined structure of Player and Match

--- app/src/main/java/org/chess/organization/Match.java
+++ app/src/main/java/org/chess/organization/Match.java
@@ -1,5 +1,38 @@
 package org.chess.organization;
 
+import org.chess.dataTypes.Colour;
+import org.chess.dataTypes.End;
+import java.util.ArrayList;
+import java.util.List;
+import org.chess.dataTypes.Move;
+import org.chess.dataTypes.Position;
+import org.chess.pieces.Piece;
+
 public class Match {
-    
+    private final ChessBoard board;
+    private final Player whitePlayer;
+    private final Player blackPlayer;
+    private List<Move> moveHistory;
+    private Player currentPlayerTurn;
+    private End state;
+
+    // Costruttore
+    public Match(Player white, Player black) {
+        
+        this.whitePlayer = white;
+        this.blackPlayer = black;
+        this.board = new ChessBoard();
+        this.currentPlayerTurn = white;          
+        this.state = End.IN_PROGRESS;
+        this.moveHistory = new ArrayList<>();
+
+        whitePlayer.setChessBoard(board);
+        blackPlayer.setChessBoard(board);
+    }
+
+    // TODO: public update move list
+    // TODO: public Player getCurrentPlayerTurn(){}
+    // TODO : public void setCurrentPlayerTurn(){}
+
+
 }
--- app/src/test/java/org/chess/PlayerTest.java
+++ app/src/test/java/org/chess/PlayerTest.java
@@ -8,21 +8,13 @@ public class PlayerTest {
     
 
     @Test
-    void testPlayerCreation() {
+    void testSubmitHumanMove() {
         
         //Player playerOne = new Player("Player 1", "white");
         //assertNotNull(playerOne);
         //assertEquals("Player 1", playerOne.getName());
 
 
-    }
-
-    @Test
-    void testPlayerCreationWithInvalidName() {
-
-        //assertThrows(IllegalArgumentException.class, () -> new Player(null, "white"));
-        //assertThrows(IllegalArgumentException.class, () -> new Player("", "white"));
-        //assertThrows(IllegalArgumentException.class, () -> new Player("   ","white"));
     }
 
 
