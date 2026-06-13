9bbbf770e77e412fa8ba597e8c46ddc46b2e18f3
SasaPahor <sasa.pahor@studenti.units.it>
2026-06-13 13:51:25 +0200

    added computeAllLegalMoves to Player Agent

--- app/src/main/java/org/chess/organization/Player.java
+++ app/src/main/java/org/chess/organization/Player.java
@@ -1,10 +1,13 @@
 package org.chess.organization;
 
-import org.chess.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.chess.dataTypes.Colour;
 import org.chess.dataTypes.Move;
 import org.chess.dataTypes.Position;
+import org.chess.pieces.Piece;
 
 public abstract class Player {
 
@@ -16,6 +19,19 @@ public abstract class Player {
         this.colour=colour;
         this.board=board;
     }
-    public abstract Move submitMove(Piece selectedPiece, Position to);
+
+    protected List<Piece> getAlivePieces(){
+        List<Piece> pieces = new ArrayList<>();
+        for(int i = 0; i<8; i++){
+            for(int j = 0; j<8; j++){
+                Piece piece=board.getPiece(new Position(i,j));
+                if(piece != null && piece.getColour() == colour)
+                    pieces.add(piece);
+            }
+        }
+        return pieces;
+    }
+
+    public abstract Move decideMove();
 
 }
