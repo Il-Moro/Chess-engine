39bfe1d52730b676e4de39f752a6287e64b4a8b6
Il-Moro <filmor04@gmail.com>
2026-06-18 10:54:51 +0200

    update di Match

--- app/src/main/java/org/chess/organization/Match.java
+++ app/src/main/java/org/chess/organization/Match.java
@@ -1,12 +1,8 @@
 package org.chess.organization;
 
-import org.chess.dataTypes.Colour;
-import org.chess.dataTypes.End;
+import org.chess.dataTypes.*;
 import java.util.ArrayList;
 import java.util.List;
-import org.chess.dataTypes.Move;
-import org.chess.dataTypes.Position;
-import org.chess.pieces.Piece;
 
 public class Match {
     private final ChessBoard board;
@@ -18,18 +14,62 @@ public class Match {
 
     // Costruttore
     public Match(Player white, Player black) {
-        
+
         this.whitePlayer = white;
         this.blackPlayer = black;
-        this.board = new ChessBoard();
-        this.currentPlayerTurn = white;          
+        this.board = new ChessBoard(true);
+        this.currentPlayerTurn = white;
         this.state = End.IN_PROGRESS;
         this.moveHistory = new ArrayList<>();
     }
 
-    // TODO: public update move list
-    // TODO: public Player getCurrentPlayerTurn(){}
-    // TODO : public void setCurrentPlayerTurn(){}
+    // returns the chess board
+    public ChessBoard getBoard() {
+        return board;
+    }
 
+    // switches the current player turn
+    public void switchTurn() {
+        currentPlayerTurn = (currentPlayerTurn == whitePlayer) ? blackPlayer : whitePlayer;
+    }
 
+    // sets the current game state
+    public void setState(End state) {
+        this.state = state;
+    }
+
+    // returns the current game state
+    public End getState() {
+        return state;
+    }
+
+    // adds a move to the move history
+    public void updateMoveHistory(Move move) {
+        moveHistory.add(move);
+    }
+
+    // returns the move history
+    public List<Move> getMoveHistory() {
+        return moveHistory;
+    }
+
+    // returns the current player turn
+    public Player getCurrentPlayerTurn() {
+        return currentPlayerTurn;
+    }
+
+    // sets the current player turn based on the colour
+    public void setCurrentPlayerTurn(Colour colour) {
+        currentPlayerTurn = (colour == Colour.WHITE) ? whitePlayer : blackPlayer;
+    }
+
+    // returns the white player
+    public Player getWhitePlayer() {
+        return whitePlayer;
+    }
+
+    // returns the black player
+    public Player getBlackPlayer() {
+        return blackPlayer;
+    }
 }
